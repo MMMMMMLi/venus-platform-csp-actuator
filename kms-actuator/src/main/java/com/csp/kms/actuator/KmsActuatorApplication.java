96061cdc0837d40eb1dbc6c7457853c16479114d
Weijia Jiang <jiang_weijia@venusgroup.com.cn>
2023-09-26 14:34:10 +0800

    feat: init code

---
+++ kms-actuator/src/main/java/com/csp/kms/actuator/KmsActuatorApplication.java
@@ -0,0 +1,14 @@
+package com.csp.kms.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class KmsActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmsActuatorApplication.class, args);
+    }
+
+}
---
+++ kms-actuator/src/test/java/com/csp/kms/actuator/KmsActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csp.kms.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmsActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
