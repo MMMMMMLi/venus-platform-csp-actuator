1b57db6c7bab212b8bda49d0f9807839110af2d1
Weijia Jiang <jiang_weijia@venusgroup.com.cn>
2023-10-17 09:54:02 +0800

    feat: 增加API常量类

--- actuator-api/src/main/java/com/csp/actuator/api/constants/ActuatorAPIConstant.java
+++ actuator-api/src/main/java/com/csp/actuator/api/constants/ActuatorAPIConstant.java
@@ -12,4 +12,6 @@ public class ActuatorAPIConstant {
     public static final String GENERATE_KEY = "/csp/actuator/api/generateKey";
     public static final String DESTROY_KEY = "/csp/actuator/api/destroyKey";
     public static final String SYNC_KEY = "/csp/actuator/api/syncKey";
+    public static final String INIT_LMK = "/csp/actuator/api/initLMK";
+    public static final String SYNC_LMK = "/csp/actuator/api/syncLMK";
 }
